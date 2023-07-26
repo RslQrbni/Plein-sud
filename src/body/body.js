5f980b07f01482051b170d97263c449a0cf0f5fb
Salsal <114516410+RslQrbni@users.noreply.github.com>
2023-07-26 17:17:14 +0200

    header and footer

---
+++ src/body/body.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './body.scss';
+
+const body=() =>{
+    return (
+        <div className="body_div">
+            
+        </div>
+    )
+}
+export default body;
